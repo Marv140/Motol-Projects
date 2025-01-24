de043934e0a2178ac9a3e3063e56b89375615a66
Marv140 <50950885+Marv140@users.noreply.github.com>
2025-01-24 20:52:19 +0100

    fix

--- code_guesser_game/app.py
+++ code_guesser_game/app.py
@@ -48,4 +48,4 @@ def submit_answer():
         return jsonify({"result": "incorrect", "correct_language": correct_language, "score": session['score']})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True, port="25571")
